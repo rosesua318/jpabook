6f6e5030d38f64516dae566a1b91386392719263
rosesua318 <rosesua@duksung.ac.kr>
2023-04-20 17:45:09 +0900

    [#2] 자바 ORM 표준 JPA 프로그래밍 2.1~2.5 완료

---
+++ jpa-start/src/main/java/jpabook/start/JpaStartApplication.java
@@ -0,0 +1,13 @@
+package jpabook.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaStartApplication.class, args);
+	}
+
+}
