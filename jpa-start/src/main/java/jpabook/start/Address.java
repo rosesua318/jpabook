10c186ca16939e2ecbf9e32377fdc18ed1d2836f
rosesua318 <rosesua@duksung.ac.kr>
2023-06-21 11:55:56 +0900

    [#56] 자바 ORM 표준 JPA 프로그래밍 9장 9.3 진도 완료

--- jpa-start/src/main/java/jpabook/start/Address.java
+++ jpa-start/src/main/java/jpabook/start/Address.java
@@ -11,4 +11,18 @@ public class Address {
     String street;
     String state;
     @Embedded Zipcode zipcode; // 임베디드 타입 포함
+
+    protected Address() {} // JPA에서 기본 생성자는 필수다.
+
+    // 생성자로 초기 값을 설정한다.
+    public Address(String city) {
+        this.city = city;
+    }
+
+    // 접근자(Getter)는 노출한다.
+    public String getCity() {
+        return city;
+    }
+
+    // 수정자(Setter)는 만들지 않는다.
 }
